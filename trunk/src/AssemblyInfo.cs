ad69d2c4afc131bb3ec6c139851eb280248beadc
anmar <anmar@d24cb6f0-7a1f-0410-a64c-9fc4892c6eea>
2006-12-28 09:14:03 +0000

    Version number set to 0.9.10.1

--- trunk/src/AssemblyInfo.cs
+++ trunk/src/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright (C) 2003-2006 Matt Howlett and others")]
 [assembly: AssemblyTrademark("This program is under The BSD license")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.9.10.0")]
+[assembly: AssemblyVersion("0.9.10.1")]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyKeyFile(@"NPlot-key.snk")]
 
